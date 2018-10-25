757c3518760a47162155c8e28cb02887058cbb21
Utilisateur <user@debian.arcahe.ovh>
2018-10-25 12:41:24 +0200

    TD4 n stuff

--- TD4/Exo1/Cor.cpp
+++ TD4/Exo1/Cor.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 using namespace std;
-#include <stdlib.h>
-#include <math.h>
-#include <CString.h>
-#include <vector.h>
 
 template <class T>
 T GetMax(T x, T y)
 {
 	return x > y ? x : y;
+}
+
+int main()
+{
+	int i=5, j=6, k;
+	float l=10.0, m=5.0, n;
+	k = GetMax(i, j);
+	n = GetMax(l, m); 
+	
+	cout << k << endl;
+	cout << n << endl;
+	return 0 ; 
 }
--- TD4/Exo2/Cor.cpp
+++ TD4/Exo2/Cor.cpp
@@ -1,4 +1,9 @@
-//Yo yo yo, add the necessary includes future me !
+#include <iostream>
+using namespace std;
+#include <stdlib.h>
+#include <math.h>
+#include "../CString.cpp"
+#include "../vector.h"
 
 template <class T>
 class Pair;
@@ -28,8 +33,60 @@ T Pair<T>::GetMax()
 	return x1 > x2 ? x1 : x2;
 }
 
+//need template ? No sauf pour certain compilo dans quel cas on fait:
+template <>
 int Pair<int>::GetMax()
 {
 	cout<<"max int ";
 	return x1 > x2 ? x1 : x2;
+}
+
+template <>
+float Pair<float>::GetMax()
+{
+	cout<<"max float :";
+	return x1 > x2 ? x1 : x2;
+}
+
+template <class T>
+void DispMax(Pair<T> &p)
+{
+	cout << p.GetMax();
+}
+
+void DispMax(Pair<CString> &p)
+{
+	cout<<p.GetMax().getString();
+}
+
+void Dispx1(Pair<CString> p)
+{
+	cout << p.x1.getString();
+}
+
+template <class T>
+ostream &operator<<(ostream& out, Pair<T> &p)
+{
+	out<<p.x1<<", "<<p.x2;
+	return out;
+}
+
+ostream & operator <<(ostream& out, Pair<CString> &p)
+{
+	cout<<"spec :";
+	out<<p.x1.getString()<<", "<<p.x2.getString();
+	return out;
+}
+
+int main(){
+	
+	 int i=5, j=6;
+	 float l=10.0, m=5.0;
+	 
+	 Pair<int> myInts (i,j); //créer une paire d’entiers
+	 Pair<float> myFloats (l,m); //créer une paire de flottants
+	 
+	 cout << myInts.GetMax()<< endl; //affiche le plus grand des 2 entiers
+	 cout << myFloats.GetMax()<<endl;//affiche le plus grand des 2 flottants
+	 return 0 ; 
 }
